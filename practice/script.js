9f7443879f96d2d302d9cb603587a4846bbfdd5c
Ryunosuke Umesaki <r.umesaki1995@gmail.com>
2026-04-06 11:28:02 +0900

    practiceにコードを記述

--- practice/script.js
+++ practice/script.js
@@ -10,3 +10,32 @@
 */
 
 const items = document.querySelectorAll('.item');
+//右のエリアを取得
+const areaRight = document.getElementById('area-right');
+//左のエリアを取得
+const areaLeft = document.getElementById('area-left');
+
+items.forEach(item => {
+    //全てのアイテムにクリックイベントを登録
+    item.addEventListener('click', (e) => {
+        let selectItem = e.target;
+        let currentArea = e.target.parentElement;
+        if (currentArea === areaLeft) {
+            //現在地が左なら右に追加
+            areaRight.append(selectItem);
+        } else {
+            //現在地が右なら左に追加
+            areaLeft.append(selectItem);
+        }
+
+        //お手本
+        // if (currentArea.id === 'area-left') {
+        //     //現在地が左なら右に追加
+        //     areaRight.append(selectItem);
+        // } else {
+        //     //現在地が右なら左に追加
+        //     areaLeft.append(selectItem);
+        // }
+    });
+});
+
